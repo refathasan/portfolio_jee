e433dc2f08be92a2135c255a53be7b4444a37bd6
Refat Hasan <hasan.refat.bd@gmail.com>
2025-03-29 14:44:28 +0600

    update master.html and HomeController

--- src/main/java/com/refathasan/portfolio/controller/HomeController.java
+++ src/main/java/com/refathasan/portfolio/controller/HomeController.java
@@ -1,24 +1,29 @@
 package com.refathasan.portfolio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping({"/","","/home"})
-    public String showHomePage(){
-        return "home";
+    public String showHomePage(Model model) {
+        model.addAttribute("title", "Home");
+        return "master";
     }
     @GetMapping("/projects")
-    public String showProjectPage(){
-        return "projects";
+    public String showProjectPage(Model model){
+        model.addAttribute("title", "Projects");
+        return "master";
     }
     @GetMapping("/resume")
-    public String showResumePage(){
-        return "resume";
+    public String showResumePage(Model model){
+        model.addAttribute("title", "Resume");
+        return "master";
     }
     @GetMapping("/contact")
-    public String showContactPage(){
-        return "contact";
+    public String showContactPage(Model model){
+        model.addAttribute("title", "Contact");
+        return "master";
     }
 }
